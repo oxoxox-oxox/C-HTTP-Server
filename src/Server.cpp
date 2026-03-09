050f74a923f73c7ca57b1911a0282d612770762b
oxoxox-oxox <jiabaocao5@gmail.com>
2026-03-09 17:19:05 +0800

    feat: add basic structure for the server

---
+++ src/Server.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main()
+{
+    // 1. establish socket
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    sockaddr_in server_addr;
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    bind(server_fd, (sockaddr *)&server_addr, sizeof(server_addr));
+
+    listen(server_fd, 5);
+
+    std::cout << "Server running on port 8080\n";
+
+    close(server_fd);
+}
